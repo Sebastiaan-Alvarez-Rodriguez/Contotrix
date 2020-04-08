0d542b43bb0a3df5145a7847bebc55b9a306b22b
Sebastiaan Alvarez Rodriguez <sebastiaanalva@gmail.com>
2020-04-08 15:24:22 +0200

    Implemented several python libraries

---
+++ installers/pyBeautifulSoup/parse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import os
+
+from bs4 import BeautifulSoup
+
+def main():
+    if len(sys.argv) < 3:
+        print('Usage: {0} <data> <repeats>'.format(sys.argv[0]))
+        return 1
+    data = sys.argv[1]
+    try:
+        repeats = int(sys.argv[2])
+    except Exception as e:
+        print('Could not convert "{0}" to number'.format(sys.argv[2]))
+        return 2
+
+    for item in os.listdir(data):
+        path = data+os.sep+item
+        with open(path, 'r') as file:
+            contents = file.read()
+        for x in range(repeats):
+            parsed = BeautifulSoup(contents)
+
+if __name__ == '__main__':
+    main()
